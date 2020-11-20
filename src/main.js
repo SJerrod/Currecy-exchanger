ad457b5f5555f5dab9af2c294d9217e647303d45
Jerrod Styrk <jstyrk@citadel.edu>
2020-11-20 15:19:27 -0800

    check amount for any non interger

--- src/main.js
+++ src/main.js
@@ -2,20 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrencyConverter from './js/converter.js';
+import CurrencyConverter from './service/converter-service.js';
 
-function showExchange(response, country2, country2Name, amount) {
-  if (response.base_code) {
+function showExchange(response, country1Name, country2, country2Name, amount) {
+  if (amount < 0 || amount.match(/[a-z]/i) || amount.length === 0) {
+    $(".money").val("");
+    alert("You must enter a number larger then 0.");
+    return false;
+  } else if (response.base_code) {
+    $(".show").append(`<li>${amount} of ${country1Name} currency is worth</li>`);
     let convertBy = 0;
     let symbol = [];
     switch (country2) {
     case ("AUD"):
       convertBy += `${response.conversion_rates.AUD}`;
-      symbol.push("A$");
+      symbol.push("$");
       break;
     case ("CAD"):
       convertBy += `${response.conversion_rates.CAD}`;
-      symbol.push("C$");
+      symbol.push("$");
       break;
     case ("EUR"):
       convertBy += `${response.conversion_rates.EUR}`;
@@ -32,15 +37,15 @@ function showExchange(response, country2, country2Name, amount) {
     }
     let total = (convertBy*amount).toFixed(2);
     console.log(symbol);
-    $(".show").append(`${symbol[0]} ${total} in ${country2Name}.`);
+    $(".show").append(`${symbol[0]}${total} in ${country2Name}.`);
   } else {
     $(".show").append(`There was an error: ${response.message}`);
   }
 }
 
-async function makeApiCall(country1, country2, country2Name, amount) {
+async function makeApiCall(country1, country1Name, country2, country2Name, amount) {
   const response = await CurrencyConverter.getConversion(country1);
-  showExchange(response, country2, country2Name, amount);
+  showExchange(response, country1Name, country2, country2Name, amount);
 }
 
 $(document).ready(function() {
@@ -50,7 +55,6 @@ $(document).ready(function() {
     let country2 = $("option[class='country2']:selected").val();
     let country2Name = $("option[class='country2']:selected").attr("name");
     let amount = $(".money").val();
-    makeApiCall(country1, country2, country2Name, amount);
-    $(".show").append(`<li>${amount} of ${country1Name} currency is worth</li>`);
+    makeApiCall(country1, country1Name, country2, country2Name, amount);
   });
 });
