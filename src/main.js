82532c0721f005759e42713da40dff5296f82acd
Jerrod Styrk <jstyrk@citadel.edu>
2020-11-20 12:03:17 -0800

    add switch case to determine country2

--- src/main.js
+++ src/main.js
@@ -4,17 +4,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyConverter from './js/converter.js';
 
-function showExchange(response) {
+function showExchange(response, country2) {
   if (response.base_code) {
-    $(".show").append(`${response.conversion_rates.AUD}`);
+    switch (country2) {
+    case ("AUD"):
+      $(".show").append(`The exchange rate is ${response.conversion_rates.AUD} per 1` );
+      break;
+    case ("CAD"):
+      $(".show").append(`The exchange rate is ${response.conversion_rates.CAD} per 1` );
+      break;
+    }
   } else {
     $(".show").append(`There was an error: ${response.message}`);
   }
 }
 
-async function makeApiCall(country1) {
+async function makeApiCall(country1, country2) {
   const response = await CurrencyConverter.getConversion(country1);
-  showExchange(response);
+  showExchange(response, country2);
 }
 
 $(document).ready(function() {
@@ -22,7 +29,7 @@ $(document).ready(function() {
     let country1 = $("option[name='country1']:selected").val();
     let country2 = $("option[name='country2']:selected").val();
     let amount = $(".money").val();
-    makeApiCall(country1);
+    makeApiCall(country1, country2);
     $(".show").append(`<li>${amount} in ${country1} is TBD in ${country2}</li>`);
   });
 });
