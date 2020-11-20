5dc4bccba3f0c62402ed2733679387fc928f446d
Jerrod Styrk <jstyrk@citadel.edu>
2020-11-20 11:33:14 -0800

    add a function to show the values retrieved from API

--- src/js/converter.js
+++ src/js/converter.js
@@ -1,15 +1,13 @@
 export default class CurrencyConverter {
-  static getConversion(country1) {
-    return fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${country1}`)
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        console.log(response.json());
-        return response.json();
-      })
-      .catch(function(error) {
-        return Error(error);
-      });
+  static async getConversion(country1) {
+    try {
+      const response = await fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${country1}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,12 +4,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyConverter from './js/converter.js';
 
+function showExchange(response) {
+  if (response.base_code) {
+    $(".show").append(`${response.conversion_rates.AUD}`);
+  } else {
+    $(".show").append(`There was an error: ${response.message}`);
+  }
+}
+
+async function makeApiCall(country1) {
+  const response = await CurrencyConverter.getConversion(country1);
+  showExchange(response);
+}
+
 $(document).ready(function() {
   $('#convert').click(function() {
     let country1 = $("option[name='country1']:selected").val();
     let country2 = $("option[name='country2']:selected").val();
     let amount = $(".money").val();
-    console.log(country1, country2, amount);
-    CurrencyConverter.getConversion(country1);
+    makeApiCall(country1);
+    $(".show").append(`<li>${amount} in ${country1} is TBD in ${country2}</li>`);
   });
 });
